3ae0ef0c0f76a302f9849fb83eab2ff8040e16b4
Michał Wawrzynowicz <michalwa2003@gmail.com>
2019-05-10 16:54:05 +0100

    Added Polish API

--- quote.py
+++ quote.py
@@ -2,19 +2,17 @@ import sys
 import re
 import json
 import requests
-from typing import List, Set, Dict
+import textwrap
+from typing import List, Set, Dict, Callable
 from argparse import ArgumentParser
 from random import choice
 
-API_URL = 'http://quotesondesign.com/wp-json/posts'
 
-
-def sanitize_quote(text: str) -> str:
-
+def sanitize_quote(quote: str) -> str:
     # Strip HTML tags
-    text = re.sub(re.compile('<.*?>'), '', text)  # type: str
+    quote = re.sub(r'<.*?>', '', quote)  # type: str
 
-    # What is the purpose of this, I'm not really sure?
+    # Replace HTML entities
     replace = {
         '&#8217;': '\'',
         '&#8216;': '\'',
@@ -31,35 +29,68 @@ def sanitize_quote(text: str) -> str:
         '\r': ''
     }
     for c in replace:
-        text = text.replace(c, replace[c])
+        quote = quote.replace(c, replace[c])
 
     # Strip leading and trailing whitespaces
-    return text.strip()
+    return quote.strip()
 
 
-def fetch_quotes(n: int, batch_size: int) -> List[str]:
-    params = {
-        'filter[orderby]': 'rand',
-        'filter[posts_per_page]': batch_size
-    }
+class SourceAPI:
+    def fetch_quotes(self, n: int, batch_size: int) -> List[str]:
+        raise NotImplementedError
 
-    # Make this a set so there are no duplicates
-    quotes = set()  # type: Set[str]
 
-    while len(quotes) < n:
-        response = requests.get(url=API_URL, params=params)
-        if response.ok:
-            for q in json.loads(response.text):
-                quotes.add(sanitize_quote(q['content']))
-                if len(quotes) >= n:
-                    break
+class QuotesOnDesignAPI(SourceAPI):
+    def __init__(self):
+        self.__url = 'http://quotesondesign.com/wp-json/posts'
 
-            print('Fetched %d/%d quotes.' % (len(quotes), n))
-        else:
-            print('Error fetching quotes.')
-            exit(1)
+    def fetch_quotes(self, n: int, batch_size: int) -> List[str]:
+        params = {
+            'filter[orderby]': 'rand',
+            'filter[posts_per_page]': batch_size
+        }
 
-    return list(quotes)
+        # Make this a set so there are no duplicates
+        quotes = set()  # type: Set[str]
+
+        while len(quotes) < n:
+            response = requests.get(url=self.__url, params=params)
+            if response.ok:
+                for q in json.loads(response.text):
+                    quotes.add(sanitize_quote(q['content']))
+                    if len(quotes) >= n:
+                        break
+
+                print('Fetched %d/%d quotes.' % (len(quotes), n))
+            else:
+                print('Error fetching quotes.')
+                exit(1)
+
+        return list(quotes)
+
+
+class DeszczowceAPI(SourceAPI):
+    def __init__(self):
+        self.__url = 'http://www.deszczowce.pl/skrypty/losowy_cytat.php'
+
+    def fetch_quotes(self, n: int, batch_size: int) -> List[str]:
+        print("NOTE: Deszczowce API doesn't support fetching in batches. Consider fetching small amounts of quotes.")
+
+        quotes = set()  # type: Set[str]
+        for i in range(n):
+            response = requests.get(url=self.__url)
+            if response.ok:
+                content = response.content.decode('iso-8859-2')
+                match = re.search(r'<i>(.*?)"</i>', content)
+                if match:
+                    quote = match.groups()[0][1:]  # type: str
+                    quotes.add(quote)
+                    print('Fetched %d/%d quotes.' % (i + 1, n))
+            else:
+                print('Error fetching quotes.')
+                exit(1)
+
+        return list(quotes)
 
 
 class Generator:
@@ -111,22 +142,34 @@ class Generator:
 
 if __name__ == '__main__':
 
+    # Dataset source APIs
+    sources = {
+        'quotesondesign': QuotesOnDesignAPI(),
+        'deszczowce':     DeszczowceAPI()
+    }
+
     # Parse options
     parser = ArgumentParser(description='Generates quotes.')
 
-    parser.add_argument('--dataset-size',       dest='dataset_size', default=200, type=int,
+    parser.add_argument('--dataset-source', '-s',     dest='dataset_source', default='quotesondesign', choices=sources.keys(),
+                        help='the API to use to fetch the source dataset')
+
+    parser.add_argument('--dataset-size', '-n',       dest='dataset_size',   default=200,              type=int,
                         help='how many quotes to fetch from the API to use as the source dataset')
 
-    parser.add_argument('--dataset-batch-size', dest='batch_size',   default=40,  type=int,
+    parser.add_argument('--dataset-batch-size', '-b', dest='batch_size',     default=40,               type=int,
                         help='how many quotes to fetch in a single request')
 
-    parser.add_argument('--ngram-length',       dest='ngram_length', default=5,   type=int,
+    parser.add_argument('--ngram-length', '-g',       dest='ngram_length',   default=5,                type=int,
                         help='length of ngrams to use for the Markov chain')
 
     args = parser.parse_args()
 
     # Generate & print
-    quotes = fetch_quotes(args.dataset_size, args.batch_size)
+    print('Fetching dataset...')
+    source = sources[args.dataset_source]
+    quotes = source.fetch_quotes(args.dataset_size, args.batch_size)
     quote = Generator(quotes, args.ngram_length).generate()
 
-    print('\n\t"%s"\n' % quote)
+    wrapped = '\n\t '.join(textwrap.wrap(quote, 80))
+    print('\n\t"%s"\n' % wrapped)
